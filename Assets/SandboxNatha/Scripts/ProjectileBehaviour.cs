92852d968ee46decedb9c0c7ab9ee51c9e09f331
natha0 <nartex92@gmail.com>
2022-11-04 20:47:19 +0100

    IA ennemi basique

---
+++ Assets/SandboxNatha/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float range = 5f;
+    private Vector3 initialPosition;
+
+    public float lifeTime = 3f;
+    private float spawnTime;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        spawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position - initialPosition).magnitude >= range || Time.time-spawnTime>lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
